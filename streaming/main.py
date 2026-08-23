97a28daf8dc4b304e53911777d2d1eba286ae51a
luchzin <luch34976@gmail.com>
2026-08-23 14:41:35 +0700

    add streaming frontend

--- streaming/main.py
+++ streaming/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+router = APIRouter()
 
+router.frontend("/", directory="dist", fallback="index.html")
+app.include_router(router, prefix="/app")
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 @app.get("/")
 def read_root():
@@ -11,3 +20,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
